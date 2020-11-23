e53e06076dee2532c7bacd440214aba78fcb6b9a
w71885717 <unlueraysegul13@gmail.com>
2020-11-23 09:48:36 +0000

    first example

--- lab6/example1.py
+++ lab6/example1.py
@@ -1,8 +1,16 @@
-email=(input("Enter an email:")
-email=email.lower()
-indx= email.index("@")
-before=email[0:indx]
-print(before)
+email = input("Please enter an email address:")
+new_mail = "ceng113@example.com"
+if "@" in email: 
+  email = email.lower()
+  part_1 = email.split("@")[0]
+  part_1 = part_1.replace(".", "")
+  part_2 = email.split("@")[1]
+  email = part_1 + "@" + part_2
+  print(email)
+  if email == new_mail:
 
-
-
+    print("True")
+  else:
+    print("False")
+else:
+  print("False")
