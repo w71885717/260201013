512e794beda36a78bff8ec3770f3983d19cf9aaf
w71885717 <unlueraysegul13@gmail.com>
2020-11-30 09:05:32 +0000

    son düzeltme books örneği

--- lab7/example2.py
+++ lab7/example2.py
@@ -2,10 +2,8 @@ books=["Ulysses","Animal FArm","Brave New World","Ender's Game"]
 book_dict={}
 first=[]
 second=[]
-total=0
 first1=[]
 second1=[]
-
 for i in books:
   for k in i:
     if k in first:
@@ -17,8 +15,6 @@ for i in books:
   second1.append(len(second))
   first.clear()
   second.clear()
-
-
 for i in range(len(books)):
   book_dict[books[i]]=(first1[i],second1[i])
 print(book_dict)
