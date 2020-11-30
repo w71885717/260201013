515558f9eb94507724baa7d1acc9382e0ffd34e9
w71885717 <unlueraysegul13@gmail.com>
2020-11-30 09:25:04 +0000

    Employees

---
+++ lab7/example4.py
@@ -0,0 +1,13 @@
+names=[]
+sallaries=[]
+employees={}
+for i in range(5):
+  name=input("what's the name:")
+  sallary=input("What's the sallary:")
+  names.append(name)
+  sallaries.append(sallary)
+for i in range(len(sallaries)):
+  employees[names[i]]=sallaries[i]
+print(employees)
+
+
